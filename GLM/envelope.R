618260b874a361cbd49c7874ed619a7f08e55440
pedroaraujo9 <pedromenezes98@gmail.com>
2019-03-10 18:56:02 -0300

    update envelope.R
    Mudanças visuais; opção de não rodar de forma paralela

--- GLM/envelope.R
+++ GLM/envelope.R
@@ -2,37 +2,48 @@ library(ggplot2)
 library(doParallel)
 library(foreach)
 
-envelope = function(modelo) {
+envelope = function(modelo, parallel = T) {
   h = influence(modelo)$hat
-  ts = resid(modelo, type="pearson")/sqrt(1-h)
-  td = resid(modelo, type="deviance")/sqrt(1-h)
-  di = (h/(1-h))*(ts^2)
   a = max(td)
   b = min(td)
   
   cl = makeCluster(detectCores())
   registerDoParallel(cl)
-
+  
   X = model.matrix(modelo)
   n = nrow(X)
   p = ncol(X)
   td = resid(modelo,type="deviance")/sqrt(1-h)
   e = matrix(0,n,100)
   
-  e = foreach(i=1:100, .combine = "cbind") %dopar% {
-    dif = runif(n) - fitted(modelo)
-    dif[dif >= 0 ] = 0
-    dif[dif<0] = 1
-    nresp = dif
-    fit <- glm(nresp ~ X, family=binomial)
-    h = influence(modelo)$hat
-    sort(resid(fit,type="deviance")/sqrt(1-h))
-    
+  if(parallel == T) {
+    e = foreach(i=1:100, .combine = "cbind") %dopar% {
+      dif = runif(n) - fitted(modelo)
+      dif[dif >= 0 ] = 0
+      dif[dif<0] = 1
+      nresp = dif
+      fit <- glm(nresp ~ X, family=binomial)
+      h = influence(modelo)$hat
+      sort(resid(fit,type="deviance")/sqrt(1-h))
+      
+    }  
+  }else{
+    e = foreach(i=1:100, .combine = "cbind") %do% {
+      dif = runif(n) - fitted(modelo)
+      dif[dif >= 0 ] = 0
+      dif[dif<0] = 1
+      nresp = dif
+      fit <- glm(nresp ~ X, family=binomial)
+      h = influence(modelo)$hat
+      sort(resid(fit,type="deviance")/sqrt(1-h))
+      
+    }
   }
+  
   
   e1 = numeric(n)
   e2 = numeric(n)
-
+  
   for(i in 1:n){
     eo = sort(e[i,])
     e1[i] = (eo[2]+eo[3])/2
@@ -54,11 +65,13 @@ envelope = function(modelo) {
   medy = qqnorm(med, plot.it = F)$y
   
   p1 = ggplot(data.frame(e1x,e1y, e2x,e2y,medx,medy,td,x,y), aes(x=x, y=y)) +
-    geom_point(alpha=0.4, size = 0.3)  + 
-    geom_line(aes(x=e1x,y=e1y), size=1, col='blue', show.legend = T) + 
-    geom_line(aes(x=e2x,y=e2y), size=1, col='blue') + 
-    geom_line(aes(x=medx,y=medy), size=1, col='grey') +
-    theme_bw() +labs(title="Gráfico de envelope", x="Percentis N(0,1)",y="Resíduos")
+    geom_point()  + 
+    geom_line(aes(x=e1x,y=e1y), size=0.5, col='black', show.legend = T) + 
+    geom_line(aes(x=e2x,y=e2y), size=0.5, col='black') + 
+    geom_line(aes(x=medx,y=medy), size=0.5, col='black', linetype='longdash') +
+    coord_equal() + 
+    theme_bw() +
+    labs(title="Gráfico de envelope", x="Percentis N(0,1)",y="Componente")
   return(p1)
 }
 
