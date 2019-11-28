ad8dd297adf8db752219f62aceeeac2390c5cdb4
Pedro Araújo <pedromenezes98@gmail.com>
2019-11-28 15:01:50 -0300

    fim do script e relatório

--- PGM/trabalho/script.R
+++ PGM/trabalho/script.R
@@ -72,15 +72,15 @@ logvero = function(deltas, beta) {
 }
 
 
-optim(par = c(1, 2, 3), logvero, hessian = T, beta = c(0.1, 0.3))
+optim(par = c(0.1, 0.2, 0.3), logvero, hessian = T, beta = c(0.1, 0.3))
 
 
 #priors
 mub0 = cbind(rep(0, 2))
-Sigb0 = cbind(c(100, 0), c(0, 100))
+Sigb0 = diag(rep(1,2))
 
 mudelta0 = cbind(rep(0, length(deltas)))
-Sigdelta0 = diag(rep(0.5, length(deltas)))
+Sigdelta0 = diag(rep(0.25, length(deltas)))
 
 
 
@@ -100,9 +100,99 @@ zi[1, ] = 0
 wi[1, ] = rexp(n) 
 
 
+
+i = 2
+#Sib B
+m = matrix(0, ncol(beta), ncol(beta))
+for(k in 1:n) {
+  m = m + (cbind(x[k,])%*%rbind(x[k,])/(wi[i-1, k]*(tau^2)))
+}
+
+Sigb = solve(m + solve(Sigb0))
+Sigb
+
+# mu B
+s = cbind(rep(0, ncol(beta)))
+for(k in 1:n) {
+  s = s + cbind(x[k,])*(zi[i-1, k] - theta*wi[i-1,k])/(wi[i-1, k]*(tau^2))
+}
+
+s
+
+
+mub = Sigb%*%(s + solve(Sigb0)%*%mub0)
+mub
+
+beta[i,] = rmvnorm(n = 1, as.numeric(mub), Sigb)
+beta[i,]
+
+#wi | .
+
+lamb = ((zi[i-1,] - as.numeric(x%*%beta[i,]))/tau)^2
+eta = ((theta/tau)^2) + 2
+
+
+wi[i,] = sapply(lamb, function(x){
+  rgig(1, param = c(x, eta, 0.5))
+})
+
+#wi[i,] = rgig(1, param = c(lamb[1], eta, 0.5))
+#wi[i,] = rginvgauss(n = n, m = sqrt(lamb/eta), 1/lamb, 0.5)
+
+hist(wi[i,])
+
+#delta |.
+
+D = optim(c(0.1, 0.2, 0.3), fn = logvero, hessian = T, beta = beta[i,])$hessian
+D = solve(D)
+D
+t = 3
+propd = delta[i-1,] + rmvnorm(1, sigma = t*D)
+propd
+deltas
+
+f1 = -logvero(propd, beta[i,])
+f2 = -logvero(delta[i-1,], beta[i,])
+
+rat = exp(f1 - f2)
+
+p1 = dmvnorm(propd, as.numeric(mudelta0), Sigdelta0)
+p2 = dmvnorm(delta[i-1,], as.numeric(mudelta0), Sigdelta0)
+
+alpha = min(c(1, rat*(p1/p2)))
+alpha
+if(runif(1) < alpha) {
+  delta[i,] = propd
+}else{
+  delta[i,] = delta[i-1,]
+}
+
+head(delta,18)
+
+
+# Z | .
+cuts = c(-Inf, 0, cumsum(exp(delta[i,])), Inf)
+pred = as.numeric(x%*%beta[i,])
+upp = numeric(n)
+low = numeric(n)
+
+for(k in 1:n) {
+  upp[k] = cuts[y[k]+1]
+  low[k] = cuts[y[k]] 
+}
+
+
+zi[i,] = rtruncnorm(n, a = low, b = upp, mean = pred + theta*wi[i,], 
+                    sd = sqrt(wi[i,]*tau^2))
+
+hist(zi[i,])
+i = i + 1
+
+
+
 i = 2
 #beta | .
-for(i in 2:200) {
+for(i in 2:100) {
   
   m = matrix(0, ncol(beta), ncol(beta))
   for(k in 1:n) {
@@ -132,17 +222,23 @@ for(i in 2:200) {
   lamb = ((zi[i-1,] - as.numeric(x%*%beta[i,]))/tau)^2
   eta = ((theta/tau)^2) + 2
   
-  wi[i,] = rginvgauss(n = n, m = sqrt(lamb/eta), 1/lamb, 0.5)
+  
+  wi[i,] = sapply(lamb, function(x){
+    rgig(1, param = c(x, eta, 0.5))
+  })
+  
+  #wi[i,] = rgig(1, param = c(lamb[1], eta, 0.5))
+  #wi[i,] = rginvgauss(n = n, m = sqrt(lamb/eta), 1/lamb, 0.5)
   
   hist(wi[i,])
   
   #delta |.
   
-  D = optim(c(1, 2, 3), fn = logvero, hessian = T, beta = beta[i,])$hessian
+  D = optim(c(0.1, 0.2, 0.3), fn = logvero, hessian = T, beta = beta[i,])$hessian
   D = solve(D)
   D
   t = 3
-  propd = delta[i-1,] + t*rmvnorm(1, sigma = D)
+  propd = delta[i-1,] + rmvnorm(1, sigma = t*D)
   propd
   deltas
   
@@ -162,7 +258,7 @@ for(i in 2:200) {
     delta[i,] = delta[i-1,]
   }
   
-  head(delta,20)
+  head(delta,18)
   
   
   # Z | .
@@ -172,15 +268,13 @@ for(i in 2:200) {
   low = numeric(n)
   
   for(k in 1:n) {
-    upp[k] = cuts[y[k]+1] - pred[k]
-    low[k] = cuts[y[k]] - pred[k]
+    upp[k] = cuts[y[k]+1]
+    low[k] = cuts[y[k]] 
   }
   
   
   zi[i,] = rtruncnorm(n, a = low, b = upp, mean = pred + theta*wi[i,], 
                       sd = sqrt(wi[i,]*tau^2))
-  
-  hist(zi[i,])
   
   cat(i, "\r")
   
